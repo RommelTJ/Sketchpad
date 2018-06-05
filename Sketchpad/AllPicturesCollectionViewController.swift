75a3adf9c4e23f39ce67c55a78fb3266859859b1
Rommel Rico <me@rommelrico.com>
2018-06-05 07:55:41 -0700

    Reuse identifier

--- Sketchpad/AllPicturesCollectionViewController.swift
+++ Sketchpad/AllPicturesCollectionViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+private let reuseIdentifier = "pictureCollectionCell"
 
 class AllPicturesCollectionViewController: UICollectionViewController {
 
