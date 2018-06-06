2b648ddfb904e1fe258285540d5780d0ae6c48bb
Rommel Rico <me@rommelrico.com>
2018-06-05 17:12:11 -0700

    Detail View Controller

---
+++ Sketchpad/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  Sketchpad
+//
+//  Created by Rommel Rico on 6/5/18.
+//  Copyright © 2018 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
