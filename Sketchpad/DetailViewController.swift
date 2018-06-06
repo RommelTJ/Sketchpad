4822ae31e212f326be2dc2a51be2bf6413cfb3f5
Rommel Rico <me@rommelrico.com>
2018-06-05 17:17:42 -0700

    Presenting the Title

--- Sketchpad/DetailViewController.swift
+++ Sketchpad/DetailViewController.swift
@@ -15,6 +15,7 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = picture?.name
     }
 
     @IBAction func deleteTapped(_ sender: Any) {
